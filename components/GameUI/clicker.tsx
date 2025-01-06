f4c4d3bed31e8a8f280fa295b0c5323ec3d6f241
Mauritonio118 <mauro.ojeda.f@gmail.com>
2025-01-06 20:47:33 -0300

    Update dependencies and enhance GameUI components
    - Upgraded Next.js from 14.2.6 to 15.1.3 in package.json and pnpm-lock.yaml.
    - Added new CSS module for button styles in clicker.module.css.
    - Enhanced Clicker component to conditionally render buttons based on user authentication status using NextAuth.
    - Enabled server-side rendering for the ErudaProvider in layout.tsx.

--- components/GameUI/clicker.tsx
+++ components/GameUI/clicker.tsx
@@ -1,3 +1,22 @@
+"use client"; 
+import styles from "./clicker.module.css"; 
+import { useSession, signIn } from "next-auth/react";
+
 export function Clicker() {
-    return <button>Click</button>;
-  }
+  const { data: session, status } = useSession();
+  const isUserLoggedIn = status === "authenticated";
+
+  return (
+    <div>
+      {isUserLoggedIn ? (
+        <button className={styles.activeButton}>
+          ¡Click para ganar!
+        </button>
+      ) : (
+        <button onClick={() => signIn()} className={styles.disabledButton}>
+          Inicia sesión con World ID para jugar
+        </button>
+      )}
+    </div>
+  );
+}
