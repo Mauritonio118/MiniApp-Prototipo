4eced066bdd81ae9b857ea92302982b26ff56092
Mauritonio118 <mauro.ojeda.f@gmail.com>
2025-01-15 20:13:00 -0300

    Implement ClickStats component and API for click data retrieval
    - Replaced Clicker with ClickStats in the main page to display total click count.
    - Added a new API endpoint to fetch initial click count from the database, validating user authentication.
    - Created ClickStats component to manage click count state and update it based on user interactions.
    - Enhanced user experience by providing real-time click updates and loading states.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { PayBlock } from "@/components/Pay";
 import { SignIn } from "@/components/SignIn";
 import { VerifyBlock } from "@/components/Verify";
-import { Clicker, Minter } from "@/components/GameUI";
+import { Minter, ClickStats } from "@/components/GameUI";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
       <SignIn />
-      <Clicker />
+      <ClickStats />
       <Minter />
       <VerifyBlock />
       <PayBlock />
