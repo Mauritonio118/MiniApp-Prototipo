d6f70bb811b5080dbfa6c3f817169cbe74b6e413
Mauritonio118 <mauro.ojeda.f@gmail.com>
2025-01-24 18:11:25 -0300

    Update MinterV1 contract, enhance GameUI Minter component, and update dependencies
    - Added the ORO contract to MinterV1.sol, implementing minting functionality with WorldID verification.
    - Enhanced the Minter component in GameUI to handle minting transactions and user sessions.
    - Updated package dependencies including dotenv, ethers, ts-node, and typescript for improved functionality and security.
    - Added test_has.ts to .gitignore to prevent tracking of test files.

--- src/components/GameUI/minter.tsx
+++ src/components/GameUI/minter.tsx
@@ -1,3 +1,42 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { MiniKit } from '@worldcoin/minikit-js'
+import { Transaction } from "ethers";
+
 export function Minter() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { data: session, status } = useSession();
+
+  const handleMint = async () => {
+    if (!session?.user) return;
+    try {
+      setIsLoading(true);
+  
+      // Obtener los datos firmados del backend
+      const response = await fetch(`/api/get-mintable`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ World_ID: session.user.name }),
+      });
+      const { mintable, timestamp } = await response.json();
+  
+      // Interactuar con el contrato
+      const tx = await MiniKit.commandsAsync.sendTransaction({
+          address: string;
+          abi: Abi ;
+          functionName: ContractFunctionName;
+          args: ContractFunctionArgs;
+
+      });
+  
+      console.log("Transacción enviada:", tx);
+    } catch (error) {
+      console.error("Error en el proceso de minting:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
     return <button>Mint</button>;
   }
