70981d7ddf532c673b4c693c9fee81645b5c2e82
Mauritonio118 <mauro.ojeda.f@gmail.com>
2025-02-08 11:37:33 -0300

    Refactor minting process with simplified WorldCoin integration and backend tracking

--- src/app/api/get-mintable/route.ts
+++ src/app/api/get-mintable/route.ts
@@ -1,41 +1,32 @@
 import { connectDB } from "@/lib/DB"; // Conexión a tu base de datos
 import { NextResponse } from "next/server";
 
+const ABI_MINT_V5 = [{"inputs":[{"internalType":"uint256","name":"amountMint","type":"uint256"}],"name":"mintToken","outputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"stateMutability":"nonpayable","type":"function"}]
+
+
 export  async function POST(request: Request) {
   try {
     const { World_ID } = await request.json();
 
     // Verificar que el usuario está registrado
     const user = await connectDB.query("SELECT * FROM users WHERE World_ID = ?", [World_ID]);
-    console.log("INFO USER")
-    console.log(typeof user)
-    console.log(user)
     if (!user) return NextResponse.json({ error: "Usuario no encontrado" }, { status: 404 });
 
+    //Extraer datos
     const { Click_Clicked, Click_Minted } = user[0];
     
-    // Calcular el monto mintable
-    console.log("Click_Clicked: ", Click_Clicked)
-    console.log("Click_Minted: ", Click_Minted)
-    console.log("Tipos: ", typeof Click_Clicked , typeof Click_Minted)
-
+    // Calcular el monto mintable en unidades de token
     const mintable = Click_Clicked - Click_Minted;
     if (mintable <= 0) return NextResponse.json({ error: "Nada que mintear" }, { status: 400 });
-    
-    console.log("MONTO MINTEABLE:", mintable)
+    const mintableOnChain = mintable * 10**18;
 
-//wrap argumentos -> todo en string
-//Multiples arg visualmente array realmente string
-
-    console.log("DATOS AL FRON:")
+    //Enviar transaccion
     return NextResponse.json({
-      address: process.env.MINTER_V4,  //Contrato testnet
-      abi: process.env.ABI_MINT_V4,
+      address: process.env.MINTER_V4,  
+      abi: ABI_MINT_V5,
       functionName: process.env.NAME_MINT_V4,
-      args: mintable.toString(),
+      args: mintableOnChain.toString(),
     });
-
-
 
   } catch (error) {
     console.error("Error en get-mintable:", error);
--- src/components/GameUI/minter.tsx
+++ src/components/GameUI/minter.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
-import { MiniKit, VerifyCommandInput, VerificationLevel, ISuccessResult, SendTransactionInput } from '@worldcoin/minikit-js'
-
-
-
+import { MiniKit, SendTransactionInput } from '@worldcoin/minikit-js'
 
 export function Minter() {
   const [isLoading, setIsLoading] = useState(false);
@@ -15,7 +12,6 @@ export function Minter() {
     try {
       setIsLoading(true);
   
-
       // Obtener los datos de minteo del backend
       const response = await fetch('/api/get-mintable', {
         method: "POST",
@@ -23,111 +19,33 @@ export function Minter() {
         body: JSON.stringify({ World_ID: session.user.name }),
       });
       const responseData = await response.json();
-      console.log("Tipo de variable:", typeof responseData.args);
-      console.log("Contenido de la variable:", responseData.args);
 
-      
-
-      //Verificar humanidad para Mintear
-      const verifyPayload: VerifyCommandInput = {
-        action: 'mintburn', // This is your action ID from the Developer Portal
-        verification_level: VerificationLevel.Orb, // Orb | Device
-      }
-
-
-      if (!MiniKit.isInstalled()) {
-        return
-      }
-      // World App will open a drawer prompting the user to confirm the operation, promise is resolved once user confirms or cancels
-      const {finalPayload} = await MiniKit.commandsAsync.verify(verifyPayload)
-        if (finalPayload.status === 'error') {
-          return console.log('Error payload', finalPayload)
-        }
-    
-        // Verify the proof in the backend
-        const verifyResponse = await fetch('/api/verify', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-          payload: finalPayload as ISuccessResult, // Parses only the fields we need to verify
-          action: 'mintburn',
-        }),
-      })
-    
-      // TODO: Handle Success!
-      const verifyResponseJson = await verifyResponse.json()
-      if (verifyResponseJson.status === 200) {
-        console.log('Verification success!')
-      }
-      
-      console.log("FINALPAYLOAD:")
-      console.log(finalPayload.proof)
-
-
-
-      const deadline = Math.floor((Date.now() + 30 * 60 * 1000) / 1000).toString()
-      const permitTransfer = {
-        permitted: {
-          token: "0xEa2B921AB2CD02667615d539Bc7fFE1F59394F99",
-          amount: '10000',
-        },
-        nonce: Date.now().toString(),
-        deadline,
-      }
-      const transa: SendTransactionInput = {
+      const transac: SendTransactionInput = {
         transaction: [
           {
             address: responseData.address,
             abi: responseData.abi,
             functionName: responseData.functionName,
-            args: [responseData.args],
-          },
-        ],
-        
-
-        // Transfers can also be at most 1 hour in the future.
-        permit2: [
-          {
-            ...permitTransfer,
-            spender: '0x34afd47fbdcc37344d1eb6a2ed53b253d4392a2f',
-          },
-        ],
-      }
-
-
-      const transa2: SendTransactionInput = {
-        transaction: [
-          {
-            address: responseData.address,
-            abi: responseData.abi,
-            functionName: responseData.functionName,
-            args: [responseData.args],
+            args: [responseData.args] ,
           },
         ],
       }
   
+    // Interactuar con el contrato
+    const { finalPayload} = await MiniKit.commandsAsync.sendTransaction(transac);
 
-    //      // Interactuar con el contrato
-    console.log("INPUT:", transa2);
-
-    const tx = await MiniKit.commandsAsync.sendTransaction(transa2);
-
-    console.log("Transacción enviada:", tx);
+    if(finalPayload.status === "success"){
+      console.log("success")
+      fetch('/api/post-minteable', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ 
+          World_ID: session.user.name,
+          amountMintedOnChain: responseData.args
+        }),
+      });
+    }
     
-
-      
-
-
-
-
-
-
-
-
-
-
     } catch (error) {
       console.error("Error en el proceso de minting:", error);
     } finally {
